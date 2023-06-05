92c5fe671631e79dff0414fbb77be215c1f3866b
burkay.sucu <burkay.sucu@forte.com.tr>
2023-06-06 00:10:25 +0300

    Examples rearranged. Running w/o args runs all examples

--- run.bat
+++
@@ -1,4 +0,0 @@
-python ./main.py -i "./Examples/NoodlesCooking/NoodlesCooking.safety1.json" -o "NoodlesCooking.safety1.debug.log" -d
-python ./main.py -i "./Examples/NuclearPlant/NuclearPlant.safety1.json" -o "NuclearPlant.safety1.debug.log" -d
-python ./main.py -i "./Examples/OperationLoop/OperationLoop.safety1.json" -o "OperationLoop.safety1.debug.log" -d
-python ./main.py -i "./Examples/Train/Train.safety1.json" -o "Train.safety1.debug.log" -d
